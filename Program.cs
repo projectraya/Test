9a144a730352a4e3cdf6d00005f875f6254ab45c
Raya <116673213+projectraya@users.noreply.github.com>
2024-03-13 11:18:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace loops_exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine(); //reads the command Go or End
+
+            while(command == "Go")
+            {
+                string first = Console.ReadLine(); //reads 3 words
+                string second = Console.ReadLine();
+                string third = Console.ReadLine();
+
+                string newWord = first + second + third; //connects them into one word
+                Console.WriteLine(newWord); //writes the new word
+
+                command = Console.ReadLine(); //reads a command again so the code continues
+            }
+            if(command == "End")
+            {
+                return; //breaks the code if the command is End
+            }
+        }
+    }
+}
