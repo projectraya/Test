255ffea4b0b4b90a12d28a84ed1dde58b64d7008
Raya <116673213+projectraya@users.noreply.github.com>
2024-03-13 11:40:15 +0200

    Add files via upload

---
+++ 10 grade - OOP/06.Implementing-Queue/06.Implementing-Queue/CircularQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Implementing_Queue
+{
+    public class CircularQueue
+    {
+        
+        private const int InitialCapacity = 16;
+
+        private int[] elements;
+        private int startIndex;
+        private int endIndex;
+        private int capacity;
+
+        public int Count { get; private set; }
+
+        public CircularQueue(int capacity)
+        {
+            // TODO
+            capacity = capacity;
+            
+        }
+
+        public void Enqueue(int element)
+        {
+            // TODO
+        }
+
+        public int Dequeue()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public int[] ToArray()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
